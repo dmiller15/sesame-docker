1cadb752ec46b5669e76875affaff6743cb3391c
Daniel Miller <dmiller15@uchicago.edu>
2018-12-10 13:18:54 -0600

    updated r script to output metadata

--- sesame-lvl3betas.R
+++ sesame-lvl3betas.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
 library(sesame)
+library(jsonlite)
 
 #    Take a folder and IDAT R/G basename from command line and return the beta values
 #    @param argv[0]: the folder containing the R&G IDAT files
@@ -11,5 +12,14 @@ args = commandArgs(trailingOnly=TRUE)
 sset <- readIDATpair(paste0(args[1],args[2]))
 sset.nb <- noob(sset)
 sset.db <- dyeBiasCorrTypeINorm(sset.nb)
+isex <- inferSex(sset.db)
+ieth <- inferEthnicity(sset.db)
 level3.betas <- getBetas(sset.db)
-write.table(level3.betas,file=paste0(args[2],"-level3betas-gdcrerun.txt"),col.names = FALSE,quote = FALSE,sep = '\t')
+ageHorv <- predictAgeHorvath353(level3.betas)
+ageSkBl <- predictAgeSkinBlood(level3.betas)
+agePhen <- predictAgePheno(level3.betas)
+metadata <- data.frame(args[1],isex,ieth,ageHorv,ageSkBl,agePhen)
+colnames(metadata) <- c("sample","inferredSex","inferredEthnicity","predictAgeHorvath","predictAgeSkinBlood","predictAgePhenotype")
+outputName = paste0(args[1],"-SesameMetadata-gdc.json")
+write(toJSON(metadata,pretty=TRUE),outputName)
+write.table(level3.betas,file=paste0(args[2],"-level3betas-gdc.txt"),col.names = FALSE,quote = FALSE,sep = '\t')
